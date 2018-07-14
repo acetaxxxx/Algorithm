209f8c893c2ca7e28767272c3613c8efc7d81d8c
Home Windows <acetaxxxx@gmail.com>
2018-07-14 11:02:35 +0800

    2. 新增Interface

---
+++ Sort/SortLib/Interface/ISort.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace SortLib.Interface
+{
+    public interface ISort<T>
+    {
+        void Sort(ref IEnumerable<T> array);
+    }
+}
